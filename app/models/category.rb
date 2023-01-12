03501e8b5929bcea7db35c194bdb1245bf100ee6
kashif tufail <kashif.tufail17@gmail.com>
2023-01-12 07:39:53 +0500

    broadcast categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,9 @@ class Category < ApplicationRecord
   has_many :discussions, dependent: :destroy
 
   scope :sorted, -> { order(name: :asc) }
-  
+
+  after_create_commit -> { broadcast_prepend_to "categories" }
+  after_update_commit -> { broadcast_replace_to "categories" }
+  after_destroy_commit -> { broadcast_remove_to "categories" }
+
 end
