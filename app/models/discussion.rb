3741d0e0a3697eccb4e58be719945d4fefcc0fa0
kashif tufail <kashif.tufail17@gmail.com>
2023-01-19 15:51:21 +0500

    users can subscribed in/out into discussions

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,23 +1,35 @@
 class Discussion < ApplicationRecord
   
+  validates :name , presence:  true
+  
   has_many :articles, dependent: :destroy
+
+
+  has_many :users, through: :articles
+  
+  has_many :subscriptions, dependent: :destroy
+  
+  has_many :opted_subscribers, -> { where(subscriptions: { subscription_type: :opted }) },
+    through: :subscriptions,
+    source: :user
+  
+    has_many :optout_subscribers, -> { where(subscriptions: { subscription_type: :optout }) },
+    through: :subscriptions,
+    source: :user
+
 
   belongs_to :category, counter_cache: true, touch: true
 
 
   belongs_to :user, default: -> { Current.user }
 
-  delegate :name, prefix: :category, to: :category, allow_nil: true
-
-  
-  validates :name , presence:  true
-
-  accepts_nested_attributes_for :articles
-
   scope :display_opened_first, -> { order(opened: :desc, updated_at: :desc) }
+
+  delegate :name, prefix: :category, to: :category, allow_nil: true   
+
+  accepts_nested_attributes_for :articles  
   
   broadcasts_to :category, inserts_by: :prepend
-
 
   after_create_commit -> { broadcast_prepend_to 'discussions'}
   after_update_commit -> { broadcast_replace_to 'discussions'}
@@ -26,5 +38,25 @@ class Discussion < ApplicationRecord
   def to_param
     "#{id}-#{name.downcase.to_s[0..90]}".parameterize
   end
+
+  def subscribed_users
+    (users + opted_subscribers).uniq - optout_subscribers
+  end
+
+  def subscription_for(user)
+    return nil if user.nil?
+    Subscription.find_by(user_id: user.id)
+  end
+
+  def toggle_subscription(user)
+    if subscription = subscription_for(user)
+      subscription.toggle!
+    elsif articles.where(user_id: user.id).any?
+      Subscription.create(user: user, subscription_type: "optout")
+    else
+      Subscription.create(user: user, subscription_type: "optin")
+    end
+  end
+
 
 end
--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -8,7 +8,14 @@ class Subscription < ApplicationRecord
   validates :subscription_type, presence: true, inclusion: { in: %w[opted optout] }
   validates :user_id, uniqueness: { scope: :discussion_id }
 
-  
+  def toggle!
+    case subscription_type
+    when "opted"
+      update(subscription_type: "optout")
+    when "optout"
+      update(subscription_type: "opted")
+    end
+  end
 
 
 
