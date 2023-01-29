485174937f507439d12a877aeda9daba69eff4ae
kashif tufail <kashif.tufail17@gmail.com>
2023-01-29 20:04:16 +0500

    avoid extra db calls

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,13 @@ class SearchController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    @discussions = Discussion.where('name ILIKE ?', "%#{params[:title_search]}%").order(created_at: :desc)
-    respond_to do |format|
+
+    if params.dig(:title_search).present?
+      @discussions = Discussion.where('name ILIKE ?', "%#{params[:title_search]}%").order(created_at: :desc)
+    else  
+      @discussions = []
+    end
+      respond_to do |format|
       format.turbo_stream do
           render turbo_stream: [
             turbo_stream.update("search_results",
